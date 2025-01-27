c0e6b2e5e7653cf31fcadcc51c9d2d42c1cacf3f
gerald274 <dariolasanchezaguilar@gmail.com>
2025-01-26 21:12:33 -0600

    avances

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+function Mensaje(){
+  return(
+    <div>soy un componente</div>
+  )
+}
+
 
 function App() {
+  const datos = [
+    {
+      nombre: "danny",
+      apellido: "cambrano"
+    }
+  ];
+
+  const mensajeSaludo = () => {
+    alert("hola a todos");
+    console.log("Soy el click");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ejemplo/>
+      <Mensaje/>
+      <h1>Hola mundo</h1>
+      <h1>Nombre: {datos[0].nombre}</h1>
+      <h1>Apellido: {datos[0].apellido}</h1>
+      <button onClick={mensajeSaludo}>Click aquí</button>
+      <ejemplo/>
     </div>
   );
 }
