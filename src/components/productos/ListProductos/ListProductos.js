ceb42cae1bdfba6f969ea574e3faed23becbab00
Gerald Oswaldo <dariolasanchezaguilar@gmail.com>
2025-01-30 17:50:35 -0600

    avance

--- src/components/productos/ListProductos/ListProductos.js
+++ src/components/productos/ListProductos/ListProductos.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import {Stack} from "react-bootstrap";
+import {ItemProductos} from "../itemProductos"
+import {Productos} from "../../../utils"
 
 export  function Productos() {
   return (
-    <div>Productos</div>
+    <Stack gap={3}>
+      <div className="p-2 center" style={{textAlign:"center"}}>
+      <h1>Lista de productos</h1>
+      <div className='target'>
+        {
+          Productosroductos.map((productos)=>(
+            <ItemProductos />
+          ))
+        }
+    
+
+      </div>
+      </div>
+      
+    </Stack>
   )
 }
+
+export default Productos;
---
+++ src/utils/productos.js
@@ -0,0 +1,20 @@
+export const Productos=[{
+    nombre: "Coca cola",
+    descripcion: "Refresco embotellado",
+    precio: 50,
+    unidad: "3L"
+},
+{
+    nombre: "Sabritas",
+    descripcion: "Comida chatarra",
+    precio: 18,
+    unidad: "70 gr"    
+},
+{
+    nombre: "galleta",
+    descripcion: "Comida chatarra con mayor calorias",
+    precio: 25,
+    unidad: "150gr"    
+}
+
+]
