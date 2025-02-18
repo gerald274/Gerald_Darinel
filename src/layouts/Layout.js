33fd2b9fdc0f6d04e02d8c9b083d9f2ba087216e
Gerald Oswaldo <dariolasanchezaguilar@gmail.com>
2025-02-18 14:23:53 -0600

    avances

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -6,14 +6,14 @@ import { Footer } from "../pages/Footer"
 export function Layout({children}) {
   return (
     <div>
-        <div className='container'>
-        <div className='init'>
-        <Menu/>
-        </div> 
-        <div className='body'>{children}</div>
-        <div className='footer'>
+     <div className='container'>
+         <div className='init'>
+         <Menu/>
+    </div> 
+    <div className='body'>{children}</div>
+    <div className='footer'>
         <Footer />
-        </div>
+    </div>
     </div>
     </div>
   );
